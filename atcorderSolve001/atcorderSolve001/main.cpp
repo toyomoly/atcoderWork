cc9e3f927bf6c5af8859832e652409000eae74cc
toyomoly <toyomoly@gmail.com>
2015-06-27 19:54:35 +0800

    temp

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,16 +2,32 @@
 using namespace std;
 int main()
 {
-    // 整数の入力
-    int a;
-    cin >> a;
-    // スペース区切りの整数の入力
-    int b,c;
-    cin >> b >> c;
-    // 文字列の入力
-    string s;
-    cin >> s;
-    // 出力
-    cout << (a+b+c) << " " << s << endl;
+    int N, R;
+    cin >> N >> R;
+    
+    string S;
+    cin >> S;
+
+    int T = 0;
+    
+    
+    // idx(N-1)から見て行く
+    // oならOK
+    if (S[N-1] == 'o') {
+        
+    } else {
+        if (N-1<=R) {
+            // 発射で終わり
+            T++;
+        }else{
+            // 進む必要がある
+        }
+    }
+    
+    
+    // idx(N-2)を見る
+    
+    
+    cout << T << endl;
     return 0;
 }
