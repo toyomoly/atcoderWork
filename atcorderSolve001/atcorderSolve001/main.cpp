3bd98615946704540e6ea325571ce7599e202920
toyomoly <toyomoly@gmail.com>
2015-07-04 21:44:07 +0800

    AC

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,16 +2,38 @@
 using namespace std;
 int main()
 {
-    // 整数の入力
-    int a;
-    cin >> a;
-    // スペース区切りの整数の入力
-    int b,c;
-    cin >> b >> c;
-    // 文字列の入力
-    string s;
-    cin >> s;
-    // 出力
-    cout << (a+b+c) << " " << s << endl;
+    int N, M;
+    cin >> N >> M;
+
+    int a[N][M];
+    int b[N][M];
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            a[i][j] = 0;
+            char c;
+            cin >> c;
+            b[i][j] = (int)(c - '0');
+        }
+    }
+    for (int i=0; i<N-1; i++) {
+        for (int j=0; j<M-1; j++) {
+            if (b[i][j] > 0) {
+                if (j == 0) {
+                    cout << "okasii" << endl;
+                } else {
+                    a[i+1][j] = b[i][j];
+                    b[i+1][j-1] -= b[i][j];
+                    b[i+1][j+1] -= b[i][j];
+                    b[i+2][j] -= b[i][j];
+                }
+            }
+        }
+    }
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
