1ff6cb2e56fca58c4ffd6371c1574b45d38d1b40
toyomoly <toyomoly@gmail.com>
2015-07-11 20:42:15 +0800

    AC

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,39 +1,25 @@
 #include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int N, M;
-    cin >> N >> M;
+    int N;
+    cin >> N;
 
-    int a[N][M];
-    int b[N][M];
+    int a[N];
     for (int i=0; i<N; i++) {
-        for (int j=0; j<M; j++) {
-            a[i][j] = 0;
-            char c;
-            cin >> c;
-            b[i][j] = (int)(c - '0');
-        }
+        cin >> a[i];
     }
-    for (int i=0; i<N-1; i++) {
-        for (int j=0; j<M-1; j++) {
-            if (b[i][j] > 0) {
-                if (j == 0) {
-                    cout << "okasii" << endl;
-                } else {
-                    a[i+1][j] = b[i][j];
-                    b[i+1][j-1] -= b[i][j];
-                    b[i+1][j+1] -= b[i][j];
-                    b[i+2][j] -= b[i][j];
-                }
-            }
-        }
-    }
+    sort(a, a+N, greater<int>());
+
+    int r2 = 0;
     for (int i=0; i<N; i++) {
-        for (int j=0; j<M; j++) {
-            cout << a[i][j];
-        }
-        cout << endl;
+        r2 += a[i] * a[i] * ((i%2 == 1) ? -1 : 1);
     }
+    
+    cout << fixed << setprecision(10) << (double)r2 * M_PI << endl;
+    
     return 0;
 }
