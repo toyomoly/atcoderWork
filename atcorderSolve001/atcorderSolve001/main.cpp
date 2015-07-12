869a9c201379c6471c2ef3394353cddc1fd5f6b2
toyomoly <toyomoly@gmail.com>
2015-07-12 10:27:27 +0800

    AC 誤差が出る問題はこれ

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <math.h>
+#include <iomanip>
 using namespace std;
+
+int A, B, C;
+
+double ft(double t) {
+    return A * t + B * sin(C * t * M_PI);
+}
+
 int main()
 {
-    // 整数の入力
-    int a;
-    cin >> a;
-    // スペース区切りの整数の入力
-    int b,c;
-    cin >> b >> c;
-    // 文字列の入力
-    string s;
-    cin >> s;
-    // 出力
-    cout << (a+b+c) << " " << s << endl;
+    cin >> A >> B >> C;
+
+    double t = 0;
+    double mx = 0;
+    
+    while (ft(mx) < 100) {
+        mx += 1000;
+    }
+    double mn = mx - 1000;
+    
+    double r = 0;
+    
+    while (!((r > 100) && (r - 100 < 0.0000001))) {
+        t = (mx + mn) / 2;
+        r = ft(t);
+        if (r > 100) {
+            mx = t;
+        } else {
+            mn = t;
+        }
+    }
+    
+    cout << fixed << setprecision(10) << t;
     return 0;
 }
