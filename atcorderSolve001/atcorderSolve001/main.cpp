051b531d468741143eeff2169ee7d2b33740b0ce
toyomoly <toyomoly@gmail.com>
2015-08-08 12:15:13 +0800

    AC

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
-    int N;
+    int N, V = 0, I = 0;
     cin >> N;
     
     for (int i=0; i<N; i++) {
-        int A, B;
-        cin >> A >> B;
-        cout << A + B << endl;
+        int A;
+        cin >> A;
+        if (V < A) {
+            V = A;
+            I = i;
+        }
     }
+    cout << I + 1 << endl;
     return 0;
 }
