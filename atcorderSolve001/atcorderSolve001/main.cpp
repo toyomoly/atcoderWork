c408c309c0e0b9fbf501e21b3f77c3f52208546e
toyomoly <toyomoly@gmail.com>
2015-08-08 11:53:09 +0800

    AC

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
 using namespace std;
-
-char get() {
-    string P, Q, R;
-    cin >> P >> Q >> R;
-    int l = (int)R.length();
-    
-    if (P == "BEGINNING") {
-        return R.at(0);
-    } else if (P == "END") {
-        return R.at(l - 1);
-    } else {
-        return R.at(l / 2);
-    }
-}
-
 int main() {
-    int N;
-    cin >> N;
-    
-    char s[N];
-    for (int i=0; i<N; i++) {
-        s[i] = get();
-    }
-
-    for (int i=0; i<N; i++) {
-        cout << s[i];
-    }
-    cout << endl;
+    cout << "ALGORITHMS" << endl;
     return 0;
 }
 
