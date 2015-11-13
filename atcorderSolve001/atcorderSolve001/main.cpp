68d09ed92f4a13cd11e8d5d0755852261d5f219b
toyomoly <toyomoly@gmail.com>
2015-11-13 15:49:37 +0900

    AC

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    // 整数の入力
-    int a;
-    cin >> a;
-    // スペース区切りの整数の入力
-    int b,c;
-    cin >> b >> c;
-    // 文字列の入力
-    string s;
-    cin >> s;
-    // 出力
-    cout << (a+b+c) << " " << s << endl;
+    double a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << ((b / a) > (d / c) ? "TAKAHASHI" :
+        (b / a) < (d / c) ? "AOKI" : "DRAW") << endl;
     return 0;
 }
