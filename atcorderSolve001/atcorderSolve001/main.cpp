7c5dfa6759323905e0c01f0fccb3027816a8190c
toyomoly <toyomoly@gmail.com>
2015-12-05 16:43:28 +0900

    1/7

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
+
+int solve(int s, int d, int x) {
+    
+    if (s == d) {
+        return x;
+    }
+    
+    if (s % 2 != 0) {
+        x++;
+    }
+    
+    if (d % 2 != 0) {
+        x++;
+    }
+    
+    return solve(s/2, d/2, x);
+}
+
 int main()
 {
-    int a, d;
-    cin >> a >> d;
-    cout << ((a < d) ? (++a * d) : (a * ++d)) << endl;
+    int n;
+    cin >> n;
+    int a[n];
+    
+    for (int i=0; i<n; i++) {
+        int s, d;
+        cin >> s >> d;
+        a[i] = solve(s, d, 0);
+    }
+    
+    for (int i=0; i<n; i++) {
+        cout << a[i] << endl;
+    }
+    
     return 0;
 }
