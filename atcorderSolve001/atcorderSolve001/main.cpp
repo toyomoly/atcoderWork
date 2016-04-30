a8c42ff61ed66c3fd68242123157363527b5d3fc
toyomoly <toyomoly@gmail.com>
2016-04-30 23:59:38 +0900

    AC 区間A~Bに含まれるの円錐の体積を求める

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,17 +1,70 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
 int main()
 {
-    // 整数の入力
-    int a;
-    cin >> a;
-    // スペース区切りの整数の入力
-    int b,c;
-    cin >> b >> c;
-    // 文字列の入力
-    string s;
-    cin >> s;
-    // 出力
-    cout << (a+b+c) << " " << s << endl;
+    // 区間A~Bの円錐の体積を求める
+    int N, Q;
+    cin >> N >> Q;
+    
+    int Xs[N], Hs[N];
+    double Vs[N];
+    
+    for (int i=0; i<N; i++) {
+        int X, R, H;
+        cin >> X >> R >> H;
+        
+        Xs[i] = X;
+        Hs[i] = H;
+        Vs[i] = 3.14159265 * R * R * H / 3.0;
+    }
+    
+    for (int j=0; j<Q; j++) {
+        int A, B;
+        cin >> A >> B;
+        
+        double Vr = 0;
+        
+        for (int i=0; i<N; i++) {
+            if (Xs[i] + Hs[i] <= A) {
+                
+            } else if (B <= Xs[i]) {
+            
+            } else if ((A <= Xs[i]) && ((Xs[i] + Hs[i]) <= B)) {
+                // 全部入ってる
+                Vr += Vs[i];
+            } else if ((Xs[i] < A) && (B < (Xs[i] + Hs[i]))) {
+                // 中だけ
+                double Hb = (Xs[i] + Hs[i]) - B;
+                double w = (Hb / Hs[i]);
+                double Vb = w * w * w * Vs[i];
+                
+                double Ha = (Xs[i] + Hs[i]) - A;
+                double q = (Ha / Hs[i]);
+                double Va = q * q * q * Vs[i];
+                
+                Vr += Va - Vb;
+            } else if ((A <= Xs[i]) && (B <= (Xs[i] + Hs[i]))) {
+                // 底面だけ入ってる
+                double Hb = (Xs[i] + Hs[i]) - B;
+                double w = (Hb / Hs[i]);
+                double Vb = w * w * w * Vs[i];
+                
+                Vr += Vs[i] - Vb;
+            } else if ((Xs[i] <= A) && ((Xs[i] + Hs[i]) <= B)) {
+                // 先端だけ入ってる
+                double Hb = (Xs[i] + Hs[i]) - A;
+                double w = (Hb / Hs[i]);
+                double Va = w * w * w * Vs[i];
+                Vr += Va;
+            }
+        }
+        
+        cout << fixed << setprecision(10) << Vr << endl;
+    }
+    
+    cout << flush;
+    
     return 0;
 }
