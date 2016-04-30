cbb647c9257703ff4c205fef40929e643331607f
toyomoly <toyomoly@gmail.com>
2016-05-01 00:22:58 +0900

    AC セグメントツリーでビットの反転と1の数を数える

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,20 +1,65 @@
 #include <iostream>
 using namespace std;
+
+int f[262144];
+int s[262144];
+
+bool chkParent(int k) {
+    if (k == 0) {
+        return f[k];
+    } else {
+        return ( f[k] != chkParent((k - 1) / 2) );
+    }
+}
+
+void update(int l, int r, int k, int st, int ed){
+    
+    if (r <= st || ed <= l) {
+        return;
+    }
+    if (l <= st && ed <= r) {
+        s[k] = (ed - st) - s[k];
+        f[k] = !f[k];
+        return;
+    }
+    
+    int md = (st + ed) / 2;
+    update(l, r, k*2+1, st, md);
+    update(l, r, k*2+2, md, ed);
+    s[k] = s[k*2+1] + s[k*2+2];
+    s[k] = f[k] ? ((ed - st) - s[k]) : s[k];
+}
+
+int get(int l, int r, int k, int st, int ed){
+    
+    if (r <= st || ed <= l) {
+        return 0;
+    }
+    if (l <= st && ed <= r) {
+        return chkParent((k - 1) / 2) ? ((ed - st) - s[k]) : s[k];
+    }
+    
+    int md = (st + ed) / 2;
+    return get(l, r, k*2+1, st, md) + get(l, r, k*2+2, md, ed);
+}
+
 int main()
 {
-    string S;
-    cin >> S;
-    int n = 0;
-    char last = 'O';
-    long l = S.length();
-    for (int i=0; i<l; i++) {
-        if (S[i] != last) {
-            last = S[i];
-            n++;
+    int n;
+    int Q;
+    cin >> n >> Q;
+    
+    for (int i=0; i<Q; i++) {
+        int q, l, r;
+        cin >> q >> l >> r;
+        if (q == 1) {
+            update(l, r, 0, 0, 131072);
+        } else {
+            cout << get(l, r, 0, 0, 131072) << endl;
         }
     }
-    int res = ((last == 'O') ? (n - 1) : n);
     
-    cout << ((res < 0) ? 0 : res) << endl;
+    cout << flush;
+    
     return 0;
 }
