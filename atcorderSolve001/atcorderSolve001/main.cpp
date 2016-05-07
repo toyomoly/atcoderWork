02c1caba8b34f61e6086b3cd12bd93ce60edc5b4
toyomoly <toyomoly@gmail.com>
2016-05-08 00:30:19 +0900

    AC グリッドを辿る経路を求める、cinが遅い

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,30 +2,31 @@
 using namespace std;
 
 int H, W;
-int A[1002][1002];
-int cash[1002][1002];
+int A[1000][1000];
+unsigned long long cash[1000][1000];
 int mod = 1000000007;
 
-int count(int x, int y) {
+unsigned long long count(int x, int y) {
     
-    int sum = 1;
+    unsigned long long sum = 1;
     
     if (cash[x][y] > 0) {
         return cash[x][y];
     }
     
-    if (A[x][y] < A[x-1][y]) {
+    if ((x != 0) && (A[x][y] > A[x-1][y])) {
         sum += count(x-1, y);
     }
-    if (A[x][y] < A[x+1][y]) {
+    if ((x != W-1) && (A[x][y] > A[x+1][y])) {
         sum += count(x+1, y);
     }
-    if (A[x][y] < A[x][y-1]) {
+    if ((y != 0) && (A[x][y] > A[x][y-1])) {
         sum += count(x, y-1);
     }
-    if (A[x][y] < A[x][y+1]) {
+    if ((y != H-1) && (A[x][y] > A[x][y+1])) {
         sum += count(x, y+1);
     }
+    sum %= mod;
     cash[x][y] = sum;
     
     return sum;
@@ -33,20 +34,22 @@ int count(int x, int y) {
 
 int main()
 {
-    memset(cash, 0, 1002 * 1002 * 4);
     cin >> H >> W;
     
     for (int y=0; y<H; y++) {
         for (int x=0; x<W; x++) {
-            scanf("%d", &A[x+1][y+1]);
+            int a;
+            scanf("%d", &a);
+            A[x][y] = a;
+            cash[x][y] = 0;
         }
     }
     
-    unsigned long sum = 0;
+    unsigned long long sum = 0;
     
     for (int x=0; x<W; x++) {
         for (int y=0; y<H; y++) {
-            sum += count(x+1, y+1);
+            sum += count(x, y);
             sum %= mod;
         }
     }
