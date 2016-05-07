9839db7204fe11b989bdf9c9a94ba30399a55de7
toyomoly <toyomoly@gmail.com>
2016-05-07 21:29:01 +0900

    AC 配列のL~RをTに書き換えた結果

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,9 +2,26 @@
 using namespace std;
 int main()
 {
-    int a, b, c;
-    cin >> a >> b >> c;
-    int d = (a < b) ? a : b;
-    cout << (c / d) << endl;
+    int N, Q;
+    cin >> N >> Q;
+    
+    int A[N];
+    
+    for (int i=0; i<N; i++) {
+        A[i] = 0;
+    }
+    
+    for (int i=0; i<Q; i++) {
+        int L, R, T;
+        cin >> L >> R >> T;
+        
+        for (int j=(L-1); j<R; j++) {
+            A[j] = T;
+        }
+    }
+
+    for (int i=0; i<N; i++) {
+        cout << A[i] << endl;
+    }
     return 0;
 }
