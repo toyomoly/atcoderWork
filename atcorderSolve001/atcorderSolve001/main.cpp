72d01a796f4cccd59864b7b9c370d7e25e386219
toyomoly <toyomoly@gmail.com>
2016-05-08 00:05:01 +0900

    test

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 using namespace std;
+
+int H, W;
+int A[1002][1002];
+int cash[1002][1002];
+int mod = 1000000007;
+
+int count(int x, int y) {
+    
+    int sum = 1;
+    
+    if (cash[x][y] > 0) {
+        return cash[x][y];
+    }
+    
+    if (A[x][y] < A[x-1][y]) {
+        sum += count(x-1, y);
+    }
+    if (A[x][y] < A[x+1][y]) {
+        sum += count(x+1, y);
+    }
+    if (A[x][y] < A[x][y-1]) {
+        sum += count(x, y-1);
+    }
+    if (A[x][y] < A[x][y+1]) {
+        sum += count(x, y+1);
+    }
+    cash[x][y] = sum;
+    
+    return sum;
+}
+
 int main()
 {
-    int H, W;
+    memset(cash, 0, 1002 * 1002 * 4);
     cin >> H >> W;
     
-    int A[W][H];
-    
-    for (int x=0; x<W; x++) {
-        for (int y=0; y<H; y++) {
-            int a;
-            cin >> a;
-            A[x][y] = a;
+    for (int y=0; y<H; y++) {
+        for (int x=0; x<W; x++) {
+            scanf("%d", &A[x+1][y+1]);
         }
     }
     
-    int sum;
+    unsigned long sum = 0;
     
     for (int x=0; x<W; x++) {
         for (int y=0; y<H; y++) {
-            // 頂上を見つける
-            if (
-                ((x == 0) || (A[x][y] > A[x-1][y]))
-                && ((x == W-1) || (A[x][y] > A[x+1][y]))
-                && ((y == 0) || (A[x][y] > A[x][y-1]))
-                && ((y == H-1) || (A[x][y] > A[x][y+1]))
-                ) {
-                // その頂点からの下る経路
-                
-                
-                
-                sum += 0;
-            }
+            sum += count(x+1, y+1);
+            sum %= mod;
         }
     }
     
