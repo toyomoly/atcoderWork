f5735b899d51e2930f47e1ea06e693e49be5fbaa
toyomoly <toyomoly@gmail.com>
2016-05-07 21:13:53 +0900

    AC 饅頭たくさん買う

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,16 +2,9 @@
 using namespace std;
 int main()
 {
-    // 整数の入力
-    int a;
-    cin >> a;
-    // スペース区切りの整数の入力
-    int b,c;
-    cin >> b >> c;
-    // 文字列の入力
-    string s;
-    cin >> s;
-    // 出力
-    cout << (a+b+c) << " " << s << endl;
+    int a, b, c;
+    cin >> a >> b >> c;
+    int d = (a < b) ? a : b;
+    cout << (c / d) << endl;
     return 0;
 }
