8dc71d36848c0440d589adbffd6720deaa1c4ec6
toyomoly <toyomoly@gmail.com>
2016-05-14 21:20:03 +0900

    AC 縦横に2つづつ並べるのは何通り

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 int main()
 {
-    int a, b, c;
-    cin >> a >> b >> c;
-    int d = (a < b) ? a : b;
-    cout << (c / d) << endl;
+    int H, W;
+    cin >> H >> W;
+    int n = 0;
+    
+    for (int i=0; i<H; i++) {
+        for (int j=1; j<W; j++) {
+            n++;
+        }
+    }
+    for (int i=0; i<W; i++) {
+        for (int j=1; j<H; j++) {
+            n++;
+        }
+    }
+    
+    cout << n << endl;
     return 0;
 }
