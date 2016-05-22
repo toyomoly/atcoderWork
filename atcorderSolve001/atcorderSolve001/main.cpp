348fc82372e1b9566b6ef5835d1032b5037e30a8
toyomoly <toyomoly@gmail.com>
2016-05-22 23:43:39 +0900

    AC 計算にかかる年数の最小値を求める

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -1,47 +1,43 @@
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 
 using namespace std;
 
+double calc (double P, double c) {
+    double t = c + (P / pow(2, c/1.5));
+    return t;
+}
+
 int main()
 {
-    double L, X, Y, S, D;
-    cin >> L >> X >> Y >> S >> D;
-
-    if (S < D) {
-        // SからDへ行く
-        // 0 < S < D < L
-        // D - S
-        // L + S - D  reverse
-        double v1 = Y + X;
-        double t1 = (D - S) / v1;
-        
-        double v2 = Y - X;
-        if (v2 > 0) {
-            double t2 = (L + S - D) / v2;
-            if (t2 < t1) {
-                t1 = t2;
-            }
-        }
-        
-        cout << fixed << setprecision(10) << t1 << endl;;
-    } else {
-        // 0 < D < S < L
-        // L + D - S
-        // S - D reverse
-        double v1 = Y + X;
-        double t1 = (L + D - S) / v1;
-        
-        double v2 = Y - X;
-        if (v2 > 0) {
-            double t2 = (S - D) / v2;
-            if (t2 < t1) {
-                t1 = t2;
-            }
-        }
-        
-        cout << fixed << setprecision(10) << t1 << endl;;
+    double P;
+    cin >> P;
+    
+    double c = 0;
+    double x = P;
+    
+    while (x > 100) {
+        c += 1.5;
+        x /= 2;
     }
+    
+    double min = c;
+    double max = c + 100;
+    double d = 100;
+    
+    for (int i=0; i<100; i++) {
+        d /= 2;
+        double t1 = calc(P, min + d);
+        double t2 = calc(P, min + d + 0.000000001);
+        if (t1 > t2) {
+            min += d;
+        } else {
+            max -= d;
+        }
+    }
+
+    cout << fixed << setprecision(10) << calc(P, min) << endl;
     
     return 0;
 }
