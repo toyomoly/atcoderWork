f79086efe365481b5cfbb31779e8505235e7aa60
toyomoly <toyomoly@gmail.com>
2016-06-19 20:33:32 +0900

    AC 白黒の画像収縮、二次元配列にしたら通ったけど、WとHの見方が違っていた

--- atcorderSolve001/atcorderSolve001/main.cpp
+++ atcorderSolve001/atcorderSolve001/main.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 int H, W;
-int a[10000];
+int a[100][100];
 
 // 周囲に0のマスがなければ収縮前の黒
 void check(int i, int j) {
-    if (a[H*i + j] == 1) {
+    if (a[i][j] == 1) {
         bool b = true;
         if (i > 0) {
-            if (j > 0) {     if (a[H*(i-1) + (j-1)] == 0) { b = false; } }
-                             if (a[H*(i-1) + (j  )] == 0) { b = false; }
-            if (j < (W-1)) { if (a[H*(i-1) + (j+1)] == 0) { b = false; } }
+            if (j > 0) {     if (a[(i-1)][(j-1)] == 0) { b = false; } }
+                             if (a[(i-1)][(j  )] == 0) { b = false; }
+            if (j < (W-1)) { if (a[(i-1)][(j+1)] == 0) { b = false; } }
         }
 
-            if (j > 0) {     if (a[H*(i  ) + (j-1)] == 0) { b = false; } }
-            if (j < (W-1)) { if (a[H*(i  ) + (j+1)] == 0) { b = false; } }
+            if (j > 0) {     if (a[(i  )][(j-1)] == 0) { b = false; } }
+            if (j < (W-1)) { if (a[(i  )][(j+1)] == 0) { b = false; } }
 
         if (i < (H-1)) {
-            if (j > 0) {     if (a[H*(i+1) + (j-1)] == 0) { b = false; } }
-                             if (a[H*(i+1) + (j  )] == 0) { b = false; }
-            if (j < (W-1)) { if (a[H*(i+1) + (j+1)] == 0) { b = false; } }
+            if (j > 0) {     if (a[(i+1)][(j-1)] == 0) { b = false; } }
+                             if (a[(i+1)][(j  )] == 0) { b = false; }
+            if (j < (W-1)) { if (a[(i+1)][(j+1)] == 0) { b = false; } }
         }
     
         if (b) {
-            a[H*i + j] = 2;
+            a[i][j] = 2;
         }
     }
 }
@@ -35,35 +35,35 @@ bool check2(int i, int j) {
     bool b = true;
     if (i > 0) {
         if (j > 0) {
-            if (a[H*(i-1) + (j-1)] > 1) { b = false; }
+            if (a[(i-1)][(j-1)] > 1) { b = false; }
         }
         
-        if (a[H*(i-1) + (j)] > 1) { b = false; }
+        if (a[(i-1)][(j)] > 1) { b = false; }
         
         if (j < (W-1)) {
-            if (a[H*(i-1) + (j+1)] > 1) { b = false; }
+            if (a[(i-1)][(j+1)] > 1) { b = false; }
         }
     }
     
     if (j > 0) {
-        if (a[H*(i) + (j-1)] > 1) { b = false; }
+        if (a[(i)][(j-1)] > 1) { b = false; }
     }
     
-    if (a[H*(i) + (j)] > 1) { b = false; }
+    if (a[(i)][(j)] > 1) { b = false; }
     
     if (j < (W-1)) {
-        if (a[H*(i) + (j+1)] > 1) { b = false; }
+        if (a[(i)][(j+1)] > 1) { b = false; }
     }
     
     if (i < (H-1)) {
         if (j > 0) {
-            if (a[H*(i+1) + (j-1)] > 1) { b = false; }
+            if (a[(i+1)][(j-1)] > 1) { b = false; }
         }
         
-        if (a[H*(i+1) + (j)] > 1) { b = false; }
+        if (a[(i+1)][(j)] > 1) { b = false; }
         
         if (j < (W-1)) {
-            if (a[H*(i+1) + (j+1)] > 1) { b = false; }
+            if (a[(i+1)][(j+1)] > 1) { b = false; }
         }
     }
     
@@ -80,7 +80,7 @@ int main()
         cin >> s;
         
         for (int j=0; j<W; j++) {
-            a[H*i + j] = ((s[j]=='#') ? 1 : 0);
+            a[i][j] = ((s[j]=='#') ? 1 : 0);
         }
     }
     
@@ -93,7 +93,7 @@ int main()
     bool possible = true;
     for (int i=0; i<H; i++) {
         for (int j=0; j<W; j++) {
-            if (a[H*i + j] == 1) {
+            if (a[i][j] == 1) {
                 if(check2(i, j)) {
                     possible = false;
                 }
@@ -106,7 +106,7 @@ int main()
         cout << "possible" << endl;
         for (int i=0; i<H; i++) {
             for (int j=0; j<W; j++) {
-                cout << ((a[H*i + j] > 1) ? '#' : '.');
+                cout << ((a[i][j] > 1) ? '#' : '.');
             }
             cout << endl;
         }
