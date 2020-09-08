412e1196ff9ba191ffed7cd7dc9d9bbb64e77588
toyomoly <toyomoly@gmail.com>
2020-09-08 11:35:51 +0900

    C

---
+++ ABCS/ConsoleApp1/ConsoleApp1/ABC117C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ABC117C
+    {
+        // https://atcoder.jp/contests/abc177/tasks/abc177_c
+        static void Main(string[] args)
+        {
+            const UInt64 m = 1000000007;
+
+            uint N = uint.Parse(Console.ReadLine());
+            var items = Console.ReadLine().Split();
+
+            UInt64[] A = Array.ConvertAll(items, UInt64.Parse);
+
+            UInt64 p = 0;
+            UInt64 sum = 0;
+
+            for (var i = N - 1; i > 0; i--)
+            {
+                p = (p + A[i]) % m;
+                sum = (sum + p * A[i - 1]) % m;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
