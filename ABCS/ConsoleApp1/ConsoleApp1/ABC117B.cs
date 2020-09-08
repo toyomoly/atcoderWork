e0f463cc23f492826feec88296af6b55e93c514d
toyomoly <toyomoly@gmail.com>
2020-09-08 10:55:43 +0900

    B

---
+++ ABCS/ConsoleApp1/ConsoleApp1/ABC117B.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ABC117B
+    {
+        // https://atcoder.jp/contests/abc177/tasks/abc177_b
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine().ToCharArray();
+            var T = Console.ReadLine().ToCharArray();
+
+            var max = 0;
+
+            for (int i = 0; i < S.Length - T.Length + 1; i++)
+            {
+                int a = comp(S, T, i);
+                if (a > max)
+                {
+                    max = a;
+                }
+            }
+
+            Console.WriteLine(T.Length - max);
+        }
+
+        static int comp(char[] S, char[] T, int i)
+        {
+            var c = 0;
+
+            for (int j = 0; j < T.Length; j++)
+            {
+                if (S[j + i] == T[j])
+                {
+                    c++;
+                }
+            }
+
+            return c;
+        }
+    }
+}
