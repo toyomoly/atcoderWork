399da0322c5d3b01096e8535225a8d7104392e79
toyomoly <toyomoly@gmail.com>
2020-09-19 23:24:22 +0900

    C問題AC

---
+++ ABCS/ConsoleApp1/ConsoleApp1/ABC179C.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ABC179C
+    {
+        // https://atcoder.jp/contests/abc179/tasks/abc179_c
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            long result = 0;
+
+            for (int A = 1; A < N; A++)
+            {
+                result += (N - 1) / A;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
