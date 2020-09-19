10d81571ebf3ec2619adcd5b30f380ae5c30cea8
toyomoly <toyomoly@gmail.com>
2020-09-19 21:08:57 +0900

    A問題AC

---
+++ ABCS/ConsoleApp1/ConsoleApp1/ABC179A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ABC179A
+    {
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+
+            if (S.Substring(S.Length - 1).Equals("s"))
+            {
+                Console.WriteLine(S + "es");
+            }
+            else
+            {
+                Console.WriteLine(S + "s");
+            }
+        }
+    }
+}
