9f15c2115df9dca5a8dc06abe6262d9067b184f4
toyomoly <toyomoly@gmail.com>
2020-09-21 14:33:11 +0900

    F問題TLE

---
+++ ABCS/ConsoleApp1/ConsoleApp1/ABC179F.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class ABC179F
+    {
+        // https://atcoder.jp/contests/abc179/tasks/abc179_f
+
+        static int[] X = new int[200001];
+        static int[] Y = new int[200001];
+        static long N;
+
+        static void Main(string[] args)
+        {
+            var items = Console.ReadLine().Split();
+            N = Int32.Parse(items[0]);
+            long Q = Int64.Parse(items[1]);
+
+            long sum = N * N - (N * 4) + 4;
+
+            for (int i = 0; i < N; i++)
+            {
+                X[i] = (int)N;
+                Y[i] = (int)N;
+            }
+
+            for (int i = 0; i < Q; i++)
+            {
+                items = Console.ReadLine().Split();
+                int Q1 = Int32.Parse(items[0]);
+                int Q2 = Int32.Parse(items[1]);
+
+                if (Q1 == 1)
+                {
+                    sum -= reverseCol(Q2);
+                }
+                else
+                {
+                    sum -= reverseRow(Q2);
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        static int reverseCol(int x)
+        {
+            int i = 2;
+
+            // reverse to Y[x]
+            while (i < Y[x])
+            {
+                X[i] = Math.Min(X[i], x);
+                i++;
+            }
+            int count = Math.Max(Y[x] - 2, 0);
+            Y[x] = 1;
+
+            return count;
+        }
+
+        static int reverseRow(int y)
+        {
+            int i = 2;
+
+            // reverse to X[y]
+            while (i < X[y])
+            {
+                Y[i] = Math.Min(Y[i], y);
+                i++;
+            }
+            int count = Math.Max(X[y] - 2, 0);
+            X[y] = 1;
+
+            return count;
+        }
+    }
+}
